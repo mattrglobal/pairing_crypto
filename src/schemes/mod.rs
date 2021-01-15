6b978657cb343b58a75d1a0ec20fb00935fcbd14
Michael Lodder <redmike7@gmail.com>
2021-01-15 15:07:23 -0700

    docs: add basic docs

--- src/schemes/mod.rs
+++ src/schemes/mod.rs
@@ -1,3 +1,6 @@
+/// Cryptographic accumulator schemes for set membership zero-knowledge proofs
 pub mod accumulator;
+/// Short group signature for the BBS+ signature
 pub mod bbs;
+/// The BLS signature scheme
 pub mod bls;
