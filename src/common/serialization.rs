456a257cb4c4c76d9146ef8e60aa9f6c1a6f25f7
Gaurav Tiwari <gaurav1@tutamail.com>
2022-06-09 01:06:02 +0530

    fix: simplify i2osp using Vec and add helper

--- src/common/serialization.rs
+++ src/common/serialization.rs
@@ -1,22 +1,55 @@
-use digest::generic_array::{ArrayLength, GenericArray};
-
 use crate::error::Error;
 
-// Implementation of I2OSP() function defined in RFC8017.
-pub(crate) fn i2osp<L: ArrayLength<u8>>(
-    integer: usize,
-) -> Result<GenericArray<u8, L>, Error> {
-    const SIZEOF_USIZE: usize = core::mem::size_of::<usize>();
-
-    // Make sure input fits in output.
-    if (SIZEOF_USIZE as u32 - integer.leading_zeros() / 8) > L::U32 {
+/// Implementation of I2OSP() function defined in RFC8017.
+// Messages can have size of (2^64-1) bytes, so support direct encoding of u64
+// prmitivies.
+pub(crate) fn i2osp(
+    integer: u64,
+    octet_length: usize,
+) -> Result<Vec<u8>, Error> {
+    if octet_length == 0 {
+        return Err(Error::Serde);
+    }
+    if integer as u128 >= (1 << (8 * octet_length) as u128) {
         return Err(Error::Serde);
     }
 
-    let mut output = GenericArray::default();
-    // copy big-endian bytes
-    output[L::USIZE.saturating_sub(SIZEOF_USIZE)..].copy_from_slice(
-        &integer.to_be_bytes()[SIZEOF_USIZE.saturating_sub(L::USIZE)..],
+    if octet_length <= core::mem::size_of::<u64>() {
+        return Ok((&integer.to_be_bytes()
+            [core::mem::size_of::<u64>() - octet_length..])
+            .to_vec());
+    }
+
+    let mut output = vec![0u8; octet_length];
+    output.splice(
+        octet_length - core::mem::size_of::<u64>()..octet_length,
+        integer.to_be_bytes().iter().cloned(),
     );
     Ok(output)
+}
+
+/// Helper function to compute "I2OSP(len(input), octent_length) || input".
+pub(crate) fn i2osp_with_data(
+    input: &[u8],
+    octent_length: usize,
+) -> Result<Vec<u8>, Error> {
+    Ok([&i2osp(input.len() as u64, octent_length)?, input].concat())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{i2osp, i2osp_with_data};
+
+    #[test]
+    fn i2osp_conversions() {
+        assert!(i2osp(1, 0).is_err());
+
+        assert!(i2osp(255, 1).is_ok());
+        assert!(i2osp(256, 1).is_err());
+
+        assert!(i2osp(1000, 2).is_ok());
+        assert!(i2osp(1000, 10).is_ok());
+
+        assert!(i2osp_with_data(&[0x01, 0x02, 0x03], 8).is_ok())
+    }
 }
