03daa01fda924db0a4c0eaed0cceaf5833cdb442
Gaurav Tiwari <gaurav1@tutamail.com>
2022-06-15 00:31:38 +0530

    feat: implement ProofGen in a single API

--- src/schemes/bbs/core.rs
+++ src/schemes/bbs/core.rs
@@ -5,6 +5,7 @@ pub(crate) mod pok_signature;
 pub(crate) mod pok_signature_proof;
 pub(crate) mod proof;
 pub(crate) mod proof_committed_builder;
+pub(crate) mod proof_gen;
 pub(crate) mod public_key;
 pub(crate) mod secret_key;
 pub(crate) mod signature;
