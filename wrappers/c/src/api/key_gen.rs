ddb82de9bc363772a94e8f36fa4f6807510245d7
Gaurav Tiwari <69981185+dev0x1@users.noreply.github.com>
2022-06-21 09:18:15 +0530

    feat: update c-bindings to use latest APIs (#41)
    * feat: update c-bindings to use latest APIs
    
    * chore: clean comment
    
    * fix(wrappers/c): update after struct rename

--- wrappers/c/src/api/key_gen.rs
+++ wrappers/c/src/api/key_gen.rs
@@ -1,56 +1,32 @@
 use crate::dtos::ByteArray;
-use ffi_support::{ByteBuffer, ExternError};
-use pairing_crypto::bls12_381::bbs::SECRET_KEY_SALT;
-use pairing_crypto::bls12_381::{PublicKey, PublicKeyVt, SecretKey};
+use ffi_support::{ByteBuffer, ErrorCode, ExternError};
+use pairing_crypto::bbs::ciphersuites::bls12_381::KeyPair;
 
-/// Generate a BLS 12-381 key pair in the G1 field.
+/// Generate a BBS BLS 12-381 curve key pair in the field.
 ///
-/// * seed: UIntArray with 32 elements
-///
-/// returned vector is the concatenation of first the private key (32 bytes)
-/// followed by the public key (48) bytes.
+/// * ikm: UInt8Array with 32 elements
+/// * key_info: UInt8Array with 32 elements
 #[no_mangle]
-pub extern "C" fn bls12381_generate_g1_key(
-    seed: ByteArray,
-    public_key: &mut ByteBuffer,
+pub extern "C" fn bls12381_generate_key_pair(
+    ikm: ByteArray,
+    key_info: ByteArray,
     secret_key: &mut ByteBuffer,
+    public_key: &mut ByteBuffer,
     err: &mut ExternError,
 ) -> i32 {
-    // Derive secret key from supplied seed otherwise generate a new seed and a derive a key from this
-    // using the underlying RNG usually defaults to the OS provided RNG e.g in Node is node crypto
-    let sk = SecretKey::new(SECRET_KEY_SALT, seed.to_opt_vec()).unwrap();
-
-    // Derive the public key from the secret key
-    let pk = PublicKey::from(&sk);
-
-    *public_key = ByteBuffer::from_vec(pk.to_bytes().to_vec());
-    *secret_key = ByteBuffer::from_vec(sk.to_bytes().to_vec());
-    *err = ExternError::success();
-    0
-}
-
-/// Generate a BLS 12-381 key pair in the G2 field.
-///
-/// * seed: UIntArray with 32 elements
-///
-/// Returned value is a byte array which is the concatenation of first the private key (32 bytes)
-/// followed by the public key (96) bytes.
-#[no_mangle]
-pub extern "C" fn bls12381_generate_g2_key(
-    seed: ByteArray,
-    public_key: &mut ByteBuffer,
-    secret_key: &mut ByteBuffer,
-    err: &mut ExternError,
-) -> i32 {
-    // Derive secret key from supplied seed otherwise generate a new seed and a derive a key from this
-    // using the underlying RNG usually defaults to the OS provided RNG e.g in Node is node crypto
-    let sk = SecretKey::new(SECRET_KEY_SALT, seed.to_opt_vec()).unwrap();
-
-    // Derive the public key from the secret key
-    let pk = PublicKeyVt::from(&sk);
-
-    *public_key = ByteBuffer::from_vec(pk.to_bytes().to_vec());
-    *secret_key = ByteBuffer::from_vec(sk.to_bytes().to_vec());
-    *err = ExternError::success();
-    0
+    // Derive secret key from supplied IKM and key information metadata.
+    if let Some(key_pair) = KeyPair::new(ikm.to_vec(), key_info.to_vec()) {
+        *secret_key =
+            ByteBuffer::from_vec(key_pair.secret_key.to_bytes().to_vec());
+        *public_key = ByteBuffer::from_vec(
+            key_pair.public_key.point_to_octets().to_vec(),
+        );
+        *err = ExternError::success();
+        return 0;
+    }
+    *err = ExternError::new_error(
+        ErrorCode::new(1),
+        "unexpected failure".to_owned(),
+    );
+    return 1;
 }
--- wrappers/c/tests/pairing_crypto_test.c
+++ wrappers/c/tests/pairing_crypto_test.c
@@ -3,57 +3,61 @@
 #include <string.h>
 #include "pairing_crypto.h"
 
+int main(int argc, char **argv)
+{
+    const uint8_t *IKM = "12345678123456781234567812345678";
+    ByteArray *ikm = (ByteArray *)malloc(sizeof(ByteArray));
+    ByteArray *key_info = (ByteArray *)malloc(sizeof(ByteArray));
+    ByteArray *secret_key = (ByteArray *)malloc(sizeof(ByteArray));
+    ByteArray *public_key = (ByteArray *)malloc(sizeof(ByteArray));
 
-int main(int argc, char** argv) {
-    ByteArray* seed = (ByteArray*) malloc(sizeof(ByteArray));
-    ByteArray* public_key = (ByteArray*) malloc(sizeof(ByteArray));
-    ByteArray* secret_key = (ByteArray*) malloc(sizeof(ByteArray));
+    ByteArray *header = (ByteArray *)malloc(sizeof(ByteArray));
 
     const int message_count = 5;
-    ByteArray* message;
-    ByteArray** messages = (ByteArray**) malloc(message_count * sizeof(ByteArray*));
-    ByteArray* signature = (ByteArray*) malloc(sizeof(ByteArray));
+    ByteArray *message;
+    ByteArray **messages = (ByteArray **)malloc(message_count * sizeof(ByteArray *));
+    ByteArray *signature = (ByteArray *)malloc(sizeof(ByteArray));
 
-    ByteArray* presentation_message = (ByteArray*) malloc(sizeof(ByteArray));
-    ByteArray* proof = (ByteArray*) malloc(sizeof(ByteArray));
+    ByteArray *presentation_message = (ByteArray *)malloc(sizeof(ByteArray));
+    ByteArray *proof = (ByteArray *)malloc(sizeof(ByteArray));
 
-    ExternError* err = (ExternError*) malloc(sizeof(ExternError));
+    ExternError *err = (ExternError *)malloc(sizeof(ExternError));
 
     uint64_t handle;
     int i;
 
-    seed->length = 0;
-    seed->data = NULL;
+    ikm->length = 32;
+    ikm->data = (uint8_t *)malloc(32);
+    memcpy((void *)ikm->data, IKM, 32);
+
+    key_info->length = 0;
+    key_info->data = NULL;
+
+    header->length = 16;
+    header->data = (uint8_t *)malloc(60);
+    memset((uint8_t *)header->data, 0xA, 16);
 
     presentation_message->length = 16;
     presentation_message->data = (uint8_t *)malloc(60);
     memset((uint8_t *)presentation_message->data, 15, 16);
 
-    printf("Create BLS12381 G2 key pair...");
+    printf("Create BLS12381 key pair...");
     fflush(stdout);
 
-    if (bls12381_generate_g2_key(*seed, (ByteBuffer*) public_key, (ByteBuffer*) secret_key, err) != 0) {
+    if (bls12381_generate_key_pair(*ikm, *key_info, (ByteBuffer *)secret_key, (ByteBuffer *)public_key, err) != 0)
+    {
         // TODO need to check the actual value of the populated public key and secret key
         printf("fail\n");
         goto Fail;
     }
     printf("pass\n");
-    
-    printf("Create BLS12381 G1 key pair...");
-    fflush(stdout);
-
-    if (bls12381_generate_g1_key(*seed, (ByteBuffer*) public_key, (ByteBuffer*) secret_key, err) != 0) {
-        // TODO need to check the actual value of the populated public key and secret key
-        printf("fail\n");
-        goto Fail;
-    }
-    printf("pass\n");
-
-    for (i = 0; i < message_count; i++) {
-        message = (ByteArray*) malloc(sizeof(ByteArray));
+#if 1
+    for (i = 0; i < message_count; i++)
+    {
+        message = (ByteArray *)malloc(sizeof(ByteArray));
         message->length = 10;
         message->data = (uint8_t *)malloc(10);
-        memset((uint8_t *)message->data, i+1, 10);
+        memset((uint8_t *)message->data, i + 1, 10);
         messages[i] = message;
     }
 
@@ -61,7 +65,8 @@ int main(int argc, char** argv) {
     fflush(stdout);
     handle = bls12381_bbs_sign_context_init(err);
 
-    if (handle == 0) {
+    if (handle == 0)
+    {
         printf("fail\n");
         goto Fail;
     }
@@ -69,7 +74,26 @@ int main(int argc, char** argv) {
 
     printf("Set secret key in sign context...");
     fflush(stdout);
-    if (bls12381_bbs_sign_context_set_secret_key(handle, *secret_key, err) != 0) {
+    if (bls12381_bbs_sign_context_set_secret_key(handle, *secret_key, err) != 0)
+    {
+        printf("fail\n");
+        goto Fail;
+    }
+    printf("pass\n");
+
+    printf("Set public key in sign context...");
+    fflush(stdout);
+    if (bls12381_bbs_sign_context_set_public_key(handle, *public_key, err) != 0)
+    {
+        printf("fail\n");
+        goto Fail;
+    }
+    printf("pass\n");
+
+    printf("Set header in sign context...");
+    fflush(stdout);
+    if (bls12381_bbs_sign_context_set_header(handle, *header, err) != 0)
+    {
         printf("fail\n");
         goto Fail;
     }
@@ -77,8 +101,10 @@ int main(int argc, char** argv) {
 
     printf("Set messages sign context...");
     fflush(stdout);
-    for (i = 0; i < message_count; i++) {
-        if (bls12381_bbs_sign_context_add_message(handle, *messages[i], err) != 0) {
+    for (i = 0; i < message_count; i++)
+    {
+        if (bls12381_bbs_sign_context_add_message(handle, *messages[i], err) != 0)
+        {
             printf("fail\n");
             goto Fail;
         }
@@ -87,25 +113,45 @@ int main(int argc, char** argv) {
 
     printf("Sign %d messages ...", message_count);
     fflush(stdout);
-    if (bls12381_bbs_sign_context_finish(handle, (ByteBuffer*)signature, err) != 0) {
+    if (bls12381_bbs_sign_context_finish(handle, (ByteBuffer *)signature, err) != 0)
+    {
         printf("fail\n");
         goto Fail;
     }
-    printf("pass\n");    
+    printf("pass\n");
 
     printf("Signature is correct size...");
-    if (signature->length != 112) { // TODO dont hardcode
+    if (signature->length != 112)
+    { // TODO dont hardcode
         printf("fail\n");
         printf("Expected %d, found %lu\n", 112, signature->length);
         goto Exit;
     }
     printf("pass\n");
 
-
     printf("Create new verify signature context...");
     fflush(stdout);
     handle = bls12381_bbs_verify_context_init(err);
-    if (handle == 0) {
+    if (handle == 0)
+    {
+        printf("fail\n");
+        goto Fail;
+    }
+    printf("pass\n");
+
+    printf("Set public key in verify signature context...");
+    fflush(stdout);
+    if (bls12381_bbs_verify_context_set_public_key(handle, *public_key, err) != 0)
+    {
+        printf("fail\n");
+        goto Fail;
+    }
+    printf("pass\n");
+
+    printf("Set header in verify context...");
+    fflush(stdout);
+    if (bls12381_bbs_verify_context_set_header(handle, *header, err) != 0)
+    {
         printf("fail\n");
         goto Fail;
     }
@@ -113,25 +159,20 @@ int main(int argc, char** argv) {
 
     printf("Set messages in verify signature context...");
     fflush(stdout);
-    for (i = 0; i < message_count; i++) {
-        if (bls12381_bbs_verify_context_set_message(handle, *messages[i], err) != 0) {
+    for (i = 0; i < message_count; i++)
+    {
+        if (bls12381_bbs_verify_context_set_message(handle, *messages[i], err) != 0)
+        {
             printf("fail\n");
             goto Fail;
         }
     }
     printf("pass\n");
 
-    printf("Set public key in verify signature context...");
-    fflush(stdout);
-    if (bls12381_bbs_verify_context_set_public_key(handle, *public_key, err) != 0) {
-        printf("fail\n");
-        goto Fail;
-    }
-    printf("pass\n");
-
     printf("Set signature in verify signature context...");
     fflush(stdout);
-    if (bls12381_bbs_verify_context_set_signature(handle, *signature, err) != 0) {
+    if (bls12381_bbs_verify_context_set_signature(handle, *signature, err) != 0)
+    {
         printf("fail\n");
         goto Fail;
     }
@@ -139,7 +180,8 @@ int main(int argc, char** argv) {
 
     printf("Verifying signature...");
     fflush(stdout);
-    if (bls12381_bbs_verify_context_finish(handle, err) != 0) {
+    if (bls12381_bbs_verify_context_finish(handle, err) != 0)
+    {
         printf("fail\n");
         goto Fail;
     }
@@ -148,7 +190,17 @@ int main(int argc, char** argv) {
     printf("Create new derive proof context...");
     fflush(stdout);
     handle = bls12381_bbs_derive_proof_context_init(err);
-    if (handle == 0) {
+    if (handle == 0)
+    {
+        printf("fail\n");
+        goto Fail;
+    }
+    printf("pass\n");
+
+    printf("Set header in proof context...");
+    fflush(stdout);
+    if (bls12381_bbs_derive_proof_context_set_header(handle, *header, err) != 0)
+    {
         printf("fail\n");
         goto Fail;
     }
@@ -156,8 +208,10 @@ int main(int argc, char** argv) {
 
     printf("Adding messages to proof context...");
     fflush(stdout);
-    for (i = 0; i < message_count; i++) {
-        if (bls12381_bbs_derive_proof_context_add_message(handle, messages[i], true, err) != 0) {
+    for (i = 0; i < message_count; i++)
+    {
+        if (bls12381_bbs_derive_proof_context_add_message(handle, messages[i], true, err) != 0)
+        {
             printf("fail\n");
             goto Fail;
         }
@@ -166,7 +220,8 @@ int main(int argc, char** argv) {
 
     printf("Setting signature in proof context...");
     fflush(stdout);
-    if (bls12381_bbs_derive_proof_context_set_signature(handle, *signature, err) != 0) {
+    if (bls12381_bbs_derive_proof_context_set_signature(handle, *signature, err) != 0)
+    {
         printf("fail\n");
         goto Fail;
     }
@@ -174,7 +229,8 @@ int main(int argc, char** argv) {
 
     printf("Set public key in proof context...");
     fflush(stdout);
-    if (bls12381_bbs_derive_proof_context_set_public_key(handle, *public_key, err) != 0) {
+    if (bls12381_bbs_derive_proof_context_set_public_key(handle, *public_key, err) != 0)
+    {
         printf("fail\n");
         goto Fail;
     }
@@ -182,7 +238,8 @@ int main(int argc, char** argv) {
 
     printf("Set presentation message in proof context...");
     fflush(stdout);
-    if (bls12381_bbs_derive_proof_context_set_presentation_message(handle, *presentation_message, err) != 0) {
+    if (bls12381_bbs_derive_proof_context_set_presentation_message(handle, *presentation_message, err) != 0)
+    {
         printf("fail\n");
         goto Fail;
     }
@@ -190,17 +247,27 @@ int main(int argc, char** argv) {
 
     printf("Creating proof...");
     fflush(stdout);
-    if (bls12381_bbs_derive_proof_context_finish(handle, (ByteBuffer*)proof, err) != 0) {
+    if (bls12381_bbs_derive_proof_context_finish(handle, (ByteBuffer *)proof, err) != 0)
+    {
         printf("fail\n");
         goto Fail;
     }
     printf("pass\n");
 
-    
     printf("Create new verify proof context...");
     fflush(stdout);
     handle = bls12381_bbs_verify_proof_context_init(err);
-    if (handle == 0) {
+    if (handle == 0)
+    {
+        printf("fail\n");
+        goto Fail;
+    }
+    printf("pass\n");
+
+    printf("Set header in proof context...");
+    fflush(stdout);
+    if (bls12381_bbs_verify_proof_context_set_header(handle, *header, err) != 0)
+    {
         printf("fail\n");
         goto Fail;
     }
@@ -208,8 +275,10 @@ int main(int argc, char** argv) {
 
     printf("Adding messages to proof context...");
     fflush(stdout);
-    for (i = 0; i < message_count; i++) {
-        if (bls12381_bbs_verify_proof_context_add_message(handle, messages[i], true, err) != 0) {
+    for (i = 0; i < message_count; i++)
+    {
+        if (bls12381_bbs_verify_proof_context_add_message(handle, messages[i], true, err) != 0)
+        {
             printf("fail\n");
             goto Fail;
         }
@@ -218,7 +287,8 @@ int main(int argc, char** argv) {
 
     printf("Setting proof in proof context...");
     fflush(stdout);
-    if (bls12381_bbs_verify_proof_context_set_proof(handle, *proof, err) != 0) {
+    if (bls12381_bbs_verify_proof_context_set_proof(handle, *proof, err) != 0)
+    {
         printf("fail\n");
         goto Fail;
     }
@@ -226,7 +296,8 @@ int main(int argc, char** argv) {
 
     printf("Set public key in proof context...");
     fflush(stdout);
-    if (bls12381_bbs_verify_proof_context_set_public_key(handle, *public_key, err) != 0) {
+    if (bls12381_bbs_verify_proof_context_set_public_key(handle, *public_key, err) != 0)
+    {
         printf("fail\n");
         goto Fail;
     }
@@ -234,17 +305,17 @@ int main(int argc, char** argv) {
 
     printf("Set presentation message in proof context...");
     fflush(stdout);
-    if (bls12381_bbs_verify_proof_context_set_presentation_message(handle, *presentation_message, err) != 0) {
+    if (bls12381_bbs_verify_proof_context_set_presentation_message(handle, *presentation_message, err) != 0)
+    {
         printf("fail\n");
         goto Fail;
     }
     printf("pass\n");
 
-    // TODO this is passing for some reason
-
     printf("Verifying proof...");
     fflush(stdout);
-    if (bls12381_bbs_verify_proof_context_finish(handle, err) != 0) {
+    if (bls12381_bbs_verify_proof_context_finish(handle, err) != 0)
+    {
         printf("fail\n");
         goto Fail;
     }
@@ -253,16 +324,17 @@ int main(int argc, char** argv) {
     printf("Tests Passed\n");
 
     goto Exit;
+#endif
 Fail:
     printf("Error Message = %s\n", err->message);
     printf("Tests Failed\n");
 Exit:
-    pairing_crypto_byte_buffer_free(*(ByteBuffer*)seed);
-    pairing_crypto_byte_buffer_free(*(ByteBuffer*)public_key);
-    pairing_crypto_byte_buffer_free(*(ByteBuffer*)secret_key);
-   
+    pairing_crypto_byte_buffer_free(*(ByteBuffer *)public_key);
+    pairing_crypto_byte_buffer_free(*(ByteBuffer *)secret_key);
+
     free(err);
-    free(seed);
+    free(ikm);
+    free(key_info);
     free(public_key);
     free(secret_key);
 }
