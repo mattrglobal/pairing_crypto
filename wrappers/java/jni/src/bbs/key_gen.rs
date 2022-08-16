7d379ca25b05f82911ae29e4ff6499fedbb1169d
Gaurav Tiwari <gaurav1@tutamail.com>
2022-08-16 23:35:58 +0530

    feat(bbs/jni): add support for more ciphersuites

---
+++ wrappers/java/jni/src/bbs/key_gen.rs
@@ -0,0 +1,78 @@
+use ffi_support::{ByteBuffer, ExternError};
+// This is the interface to the JVM that we'll
+// call the majority of our methods on.
+use jni::JNIEnv;
+
+// These objects are what you should use as arguments to your native function.
+// They carry extra lifetime information to prevent them escaping this context
+// and getting used after being GC'd.
+use jni::objects::JObject;
+
+// This is just a pointer. We'll be returning it from our function.
+// We can't return one of the objects with lifetime information because the
+// lifetime checker won't let us.
+use jni::sys::{jbyte, jbyteArray, jint};
+
+use pairing_crypto_c::{
+    bbs::key_gen::{
+        bbs_bls12_381_sha_256_generate_key_pair,
+        bbs_bls12_381_shake_256_generate_key_pair,
+    },
+    dtos::ByteArray,
+};
+
+macro_rules! bbs_key_gen_api_wrapper_generator {
+    (
+        $java_wrapper_generate_key_pair_fn:ident,
+        $generate_key_pair_fn:ident
+    ) => {
+        #[allow(non_snake_case)]
+        #[no_mangle]
+        pub extern "C" fn $java_wrapper_generate_key_pair_fn(
+            env: JNIEnv,
+            _: JObject,
+            ikm: jbyteArray,
+            key_info: jbyteArray,
+            public_key: jbyteArray,
+            secret_key: jbyteArray,
+        ) -> jint {
+            let ikm = match env.convert_byte_array(ikm) {
+                Err(_) => return 1,
+                Ok(s) => s,
+            };
+            let key_info = match env.convert_byte_array(key_info) {
+                Err(_) => return 1,
+                Ok(s) => s,
+            };
+            let mut error = ExternError::success();
+            let mut sk = ByteBuffer::from_vec(vec![]);
+            let mut pk = ByteBuffer::from_vec(vec![]);
+            let result = $generate_key_pair_fn(
+                ByteArray::from(&ikm),
+                ByteArray::from(&key_info),
+                &mut sk,
+                &mut pk,
+                &mut error,
+            );
+            if result != 0 {
+                return result;
+            }
+            let pk: Vec<i8> =
+                pk.destroy_into_vec().iter().map(|b| *b as jbyte).collect();
+            let sk: Vec<i8> =
+                sk.destroy_into_vec().iter().map(|b| *b as jbyte).collect();
+            copy_to_jni!(env, public_key, pk.as_slice());
+            copy_to_jni!(env, secret_key, sk.as_slice());
+            0
+        }
+    };
+}
+
+bbs_key_gen_api_wrapper_generator!(
+    Java_pairing_1crypto_Bbs_bbs_1bls12381_1sha256_1generate_1key_1pair,
+    bbs_bls12_381_sha_256_generate_key_pair
+);
+bbs_key_gen_api_wrapper_generator!(
+    Java_pairing_1crypto_Bbs_bbs_1bls12381_1shake256_1generate_1key_1pair,
+    bbs_bls12_381_shake_256_generate_key_pair
+);
---
+++ wrappers/java/jni/src/bbs/proof_gen.rs
@@ -0,0 +1,229 @@
+use ffi_support::{ByteBuffer, ExternError};
+use jni::{
+    objects::JObject,
+    sys::{jboolean, jbyte, jbyteArray, jint, jlong},
+    JNIEnv,
+};
+
+use crate::update_last_error;
+use pairing_crypto_c::{
+    bbs::{
+        proof_gen::{
+            bbs_bls12_381_sha_256_proof_gen_context_add_message,
+            bbs_bls12_381_sha_256_proof_gen_context_finish,
+            bbs_bls12_381_sha_256_proof_gen_context_init,
+            bbs_bls12_381_sha_256_proof_gen_context_set_header,
+            bbs_bls12_381_sha_256_proof_gen_context_set_presentation_message,
+            bbs_bls12_381_sha_256_proof_gen_context_set_public_key,
+            bbs_bls12_381_sha_256_proof_gen_context_set_signature,
+            bbs_bls12_381_shake_256_proof_gen_context_add_message,
+            bbs_bls12_381_shake_256_proof_gen_context_finish,
+            bbs_bls12_381_shake_256_proof_gen_context_init,
+            bbs_bls12_381_shake_256_proof_gen_context_set_header,
+            bbs_bls12_381_shake_256_proof_gen_context_set_presentation_message,
+            bbs_bls12_381_shake_256_proof_gen_context_set_public_key,
+            bbs_bls12_381_shake_256_proof_gen_context_set_signature,
+        },
+        BBS_BLS12381G1_PUBLIC_KEY_LENGTH,
+    },
+    dtos::ByteArray,
+};
+
+macro_rules! bbs_proof_gen_api_wrapper_generator {
+    (
+        $java_wrapper_init_fn:ident,
+        $init_fn:ident,
+        $java_wrapper_set_public_key_fn:ident,
+        $set_public_key_fn:ident,
+        $java_wrapper_set_header_fn:ident,
+        $set_header_fn:ident,
+        $java_wrapper_set_signature_fn:ident,
+        $set_signature_fn:ident,
+        $java_wrapper_set_presentation_message_fn:ident,
+        $set_presentation_message_fn:ident,
+        $java_wrapper_add_message_fn:ident,
+        $add_message_fn:ident,
+        $java_wrapper_finish_fn:ident,
+        $finish_fn:ident,
+    ) => {
+        #[allow(non_snake_case)]
+        #[no_mangle]
+        pub extern "C" fn $java_wrapper_init_fn(
+            _: JNIEnv,
+            _: JObject,
+        ) -> jlong {
+            let mut error = ExternError::success();
+            $init_fn(&mut error) as jlong
+        }
+
+        #[allow(non_snake_case)]
+        #[no_mangle]
+        pub extern "C" fn $java_wrapper_set_public_key_fn(
+            env: JNIEnv,
+            _: JObject,
+            handle: jlong,
+            public_key: jbyteArray,
+        ) -> jint {
+            match env.convert_byte_array(public_key) {
+                Err(_) => 1,
+                Ok(s) => {
+                    if s.len() != BBS_BLS12381G1_PUBLIC_KEY_LENGTH {
+                        2
+                    } else {
+                        let mut error = ExternError::success();
+                        let byte_array = ByteArray::from(&s);
+                        $set_public_key_fn(
+                            handle as u64,
+                            &byte_array,
+                            &mut error,
+                        )
+                    }
+                }
+            }
+        }
+
+        #[allow(non_snake_case)]
+        #[no_mangle]
+        pub extern "C" fn $java_wrapper_set_header_fn(
+            env: JNIEnv,
+            _: JObject,
+            handle: jlong,
+            header: jbyteArray,
+        ) -> jint {
+            match env.convert_byte_array(header) {
+                Err(_) => 1,
+                Ok(s) => {
+                    let mut error = ExternError::success();
+                    let byte_array = ByteArray::from(&s);
+                    $set_header_fn(handle as u64, &byte_array, &mut error)
+                }
+            }
+        }
+
+        #[allow(non_snake_case)]
+        #[no_mangle]
+        pub extern "C" fn $java_wrapper_set_signature_fn(
+            env: JNIEnv,
+            _: JObject,
+            handle: jlong,
+            signature: jbyteArray,
+        ) -> jint {
+            match env.convert_byte_array(signature) {
+                Err(_) => 1,
+                Ok(s) => {
+                    let mut error = ExternError::success();
+                    let byte_array = ByteArray::from(&s);
+                    let res = $set_signature_fn(
+                        handle as u64,
+                        &byte_array,
+                        &mut error,
+                    );
+                    if res != 0 {
+                        update_last_error(error.get_message().as_str());
+                    }
+                    res
+                }
+            }
+        }
+
+        #[allow(non_snake_case)]
+        #[no_mangle]
+        pub extern "C" fn $java_wrapper_set_presentation_message_fn(
+            env: JNIEnv,
+            _: JObject,
+            handle: jlong,
+            presentation_message: jbyteArray,
+        ) -> jint {
+            match env.convert_byte_array(presentation_message) {
+                Err(_) => 1,
+                Ok(s) => {
+                    let mut error = ExternError::success();
+                    let byte_array = ByteArray::from(&s);
+                    $set_presentation_message_fn(
+                        handle as u64,
+                        &byte_array,
+                        &mut error,
+                    )
+                }
+            }
+        }
+
+        #[allow(non_snake_case)]
+        #[no_mangle]
+        pub extern "C" fn $java_wrapper_add_message_fn(
+            env: JNIEnv,
+            _: JObject,
+            handle: jlong,
+            reveal: jboolean,
+            message: jbyteArray,
+        ) -> jint {
+            match env.convert_byte_array(message) {
+                Err(_) => 1,
+                Ok(s) => {
+                    let mut error = ExternError::success();
+                    let byte_array = ByteArray::from(&s);
+                    $add_message_fn(
+                        handle as u64,
+                        reveal != 0,
+                        &byte_array,
+                        &mut error,
+                    )
+                }
+            }
+        }
+
+        #[allow(non_snake_case)]
+        #[no_mangle]
+        pub extern "C" fn $java_wrapper_finish_fn(
+            env: JNIEnv,
+            _: JObject,
+            handle: jlong,
+            proof: jbyteArray,
+        ) -> jint {
+            let mut error = ExternError::success();
+            let mut p = ByteBuffer::from_vec(vec![]);
+            let res = $finish_fn(handle as u64, &mut p, &mut error);
+            if res != 0 {
+                return res;
+            }
+            let res = p.destroy_into_vec();
+            let pp: Vec<jbyte> = res.iter().map(|b| *b as jbyte).collect();
+            copy_to_jni!(env, proof, pp.as_slice());
+            0
+        }
+    };
+}
+
+bbs_proof_gen_api_wrapper_generator!(
+    Java_pairing_1crypto_Bbs_bbs_1bls12381_1sha256_1proof_1gen_1context_1init,
+    bbs_bls12_381_sha_256_proof_gen_context_init,
+    Java_pairing_1crypto_Bbs_bbs_1bls12381_1sha256_1proof_1gen_1context_1set_1public_1key,
+    bbs_bls12_381_sha_256_proof_gen_context_set_public_key,
+    Java_pairing_1crypto_Bbs_bbs_1bls12381_1sha256_1proof_1gen_1context_1set_1header,
+    bbs_bls12_381_sha_256_proof_gen_context_set_header,
+    Java_pairing_1crypto_Bbs_bbs_1bls12381_1sha256_1proof_1gen_1context_1set_1signature,
+    bbs_bls12_381_sha_256_proof_gen_context_set_signature,
+    Java_pairing_1crypto_Bbs_bbs_1bls12381_1sha256_1proof_1gen_1context_1set_1presentation_1message,
+    bbs_bls12_381_sha_256_proof_gen_context_set_presentation_message,
+    Java_pairing_1crypto_Bbs_bbs_1bls12381_1sha256_1proof_1gen_1context_1add_1message,
+    bbs_bls12_381_sha_256_proof_gen_context_add_message,
+    Java_pairing_1crypto_Bbs_bbs_1bls12381_1sha256_1proof_1gen_1context_1finish,
+    bbs_bls12_381_sha_256_proof_gen_context_finish,
+);
+
+bbs_proof_gen_api_wrapper_generator!(
+    Java_pairing_1crypto_Bbs_bbs_1bls12381_1shake256_1proof_1gen_1context_1init,
+    bbs_bls12_381_shake_256_proof_gen_context_init,
+    Java_pairing_1crypto_Bbs_bbs_1bls12381_1shake256_1proof_1gen_1context_1set_1public_1key,
+    bbs_bls12_381_shake_256_proof_gen_context_set_public_key,
+    Java_pairing_1crypto_Bbs_bbs_1bls12381_1shake256_1proof_1gen_1context_1set_1header,
+    bbs_bls12_381_shake_256_proof_gen_context_set_header,
+    Java_pairing_1crypto_Bbs_bbs_1bls12381_1shake256_1proof_1gen_1context_1set_1signature,
+    bbs_bls12_381_shake_256_proof_gen_context_set_signature,
+    Java_pairing_1crypto_Bbs_bbs_1bls12381_1shake256_1proof_1gen_1context_1set_1presentation_1message,
+    bbs_bls12_381_shake_256_proof_gen_context_set_presentation_message,
+    Java_pairing_1crypto_Bbs_bbs_1bls12381_1shake256_1proof_1gen_1context_1add_1message,
+    bbs_bls12_381_shake_256_proof_gen_context_add_message,
+    Java_pairing_1crypto_Bbs_bbs_1bls12381_1shake256_1proof_1gen_1context_1finish,
+    bbs_bls12_381_shake_256_proof_gen_context_finish,
+);
