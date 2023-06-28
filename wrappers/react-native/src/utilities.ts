4f19146c163fb949017e631eda413cd6013c34a9
Maté Antunovic <108311641+mate-from-mattr@users.noreply.github.com>
2023-06-28 12:03:06 +1200

    fix: add utility funcs to rn
    Co-authored-by: Frederick Cai <frederick.cai@mattr.global>

--- wrappers/react-native/src/utilities.ts
+++ wrappers/react-native/src/utilities.ts
@@ -52,3 +52,25 @@ export const mapObjIndexed = <K extends number | string, V, R>(
     return accu;
   }, {} as Record<K, R>);
 };
+
+interface RevealMessage<E> {
+  value: E;
+  reveal: boolean;
+}
+
+export const convertToRevealMessageArray = <E>(messages: E[], revealedIndicies: number[]): RevealMessage<E>[] => {
+  return messages.map((value: E, i: number) => ({
+    reveal: revealedIndicies.includes(i),
+    value,
+  }));
+};
+
+export const convertRevealMessageArrayToRevealMap = <E>(
+  messages: RevealMessage<E>[]
+): Record<number, RevealMessage<E>> => {
+  return messages.reduce(
+    (map: Record<number, RevealMessage<E>>, message: RevealMessage<E>, i: number) =>
+      message.reveal ? { ...map, [i]: message } : map,
+    {}
+  );
+};
