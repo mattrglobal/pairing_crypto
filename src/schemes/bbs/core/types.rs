2a59321ef76a28a5acc3100911c9418c8f4d41e9
Vasilis Kalos <kalosbasileios@gmail.com>
2024-01-14 19:08:42 +0200

    refactor: simplify traces

--- src/schemes/bbs/core/types.rs
+++ src/schemes/bbs/core/types.rs
@@ -101,9 +101,6 @@ impl RandomScalars {
     }
 }
 
-#[cfg(feature = "__private_bbs_fixtures_generator_api")]
-use crate::common::util::vec_to_byte_array;
-
 /// A struct to hold a trace of the signature generation operation
 #[derive(Debug, Clone)]
 pub struct SignatureTrace {
@@ -118,23 +115,6 @@ impl Default for SignatureTrace {
         Self {
             B: [0u8; OCTET_POINT_G1_LENGTH],
             domain: [0u8; OCTET_SCALAR_LENGTH],
-        }
-    }
-}
-
-impl SignatureTrace {
-    /// Helper function to get a new signature trace from Vec inputs
-    #[cfg_attr(
-        docsrs,
-        doc(cfg(feature = "__private_bbs_fixtures_generator_api"))
-    )]
-    #[cfg(feature = "__private_bbs_fixtures_generator_api")]
-    pub fn new_from_vec(B: Vec<u8>, domain: Vec<u8>) -> Self {
-        Self {
-            B: vec_to_byte_array::<OCTET_POINT_G1_LENGTH>(&B)
-                .expect("Invalid B"),
-            domain: vec_to_byte_array::<OCTET_SCALAR_LENGTH>(&domain)
-                .expect("Invalid domain"),
         }
     }
 }
@@ -170,35 +150,17 @@ impl Default for ProofTrace {
 }
 
 impl ProofTrace {
-    /// Helper function to deserialize the ProofTrace struct
-    #[cfg(feature = "__private_bbs_fixtures_generator_api")]
-    pub fn new_from_vec(
-        // TODO: Change to be &mut self and remove the random_scalars: default.
-        A_bar: Vec<u8>,
-        B_bar: Vec<u8>,
-        T: Vec<u8>,
-        domain: Vec<u8>,
-        challenge: Vec<u8>,
-    ) -> Self {
-        Self {
-            random_scalars: RandomScalars::default(),
-            A_bar: vec_to_byte_array::<OCTET_POINT_G1_LENGTH>(&A_bar)
-                .expect("Invalid A_bar"),
-            B_bar: vec_to_byte_array::<OCTET_POINT_G1_LENGTH>(&B_bar)
-                .expect("Invalid B_bar"),
-            T: vec_to_byte_array::<OCTET_POINT_G1_LENGTH>(&T)
-                .expect("Invalid T"),
-            domain: vec_to_byte_array::<OCTET_SCALAR_LENGTH>(&domain)
-                .expect("Invalid domain"),
-            challenge: vec_to_byte_array::<OCTET_SCALAR_LENGTH>(&challenge)
-                .expect("Invalid challenge"),
-        }
-    }
-
-    pub(crate) fn new_from_init_res(&mut self, init_res: &ProofInitResult) {
+    pub(crate) fn new_with_init_res(
+        &mut self,
+        init_res: &ProofInitResult,
+        challenge: Challenge,
+        random_scalars: RandomScalars,
+    ) {
         self.A_bar = point_to_octets_g1(&init_res.A_bar);
         self.B_bar = point_to_octets_g1(&init_res.B_bar);
         self.T = point_to_octets_g1(&init_res.T);
         self.domain = init_res.domain.to_bytes_be();
+        self.challenge = challenge.0.to_bytes_be();
+        self.random_scalars = random_scalars;
     }
 }
