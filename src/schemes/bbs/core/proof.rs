14ef10ae71ed3c7d689082cfc861fa7951f17b69
Vasilis Kalos <kalosbasileios@gmail.com>
2024-06-19 00:32:50 +0200

    fix cargo fmt

--- src/schemes/bbs/core/proof.rs
+++ src/schemes/bbs/core/proof.rs
@@ -446,13 +446,11 @@ impl Proof {
     /// Each member of `Proof` is deserialized from big-endian bytes.
     /// Expected input size is `OCTET_POINT_G1_LENGTH * 3 + OCTET_SCALAR_LENGTH
     /// * (5 + U)` where `OCTET_POINT_G1_LENGTH`, size of a point in `G1` in
-    ///   ompressed form, `OCTET_SCALAR_LENGTH`, size of a `Scalar`, and `U` is
-    ///   the number of hidden messages.
-    /// 
-    /// For BLS12-381 based implementation, OCTET_POINT_G1_LENGTH is 48 byes,
-    /// and OCTET_SCALAR_LENGTH is 32 bytes, then bytes sequence will be
-    /// treated as [48, 48, 32, 32, 32, 32*U ] to represent   
-    /// proof = (Abar, Bbar, c, r2^, z^, (m^_1, ..., m^_U)).
+    /// compressed form, `OCTET_SCALAR_LENGTH`, size of a `Scalar`, and `U` is
+    /// the number of hidden messages. For BLS12-381 based implementation,
+    /// OCTET_POINT_G1_LENGTH is 48 byes, and OCTET_SCALAR_LENGTH is 32 bytes,
+    /// then bytes sequence will be treated as [48, 48, 32, 32, 32, 32*U ] to
+    /// represent proof = (Abar, Bbar, c, r2^, z^, (m^_1, ..., m^_U)).
     pub fn from_octets<B: AsRef<[u8]>>(bytes: B) -> Result<Self, Error> {
         const PROOF_LEN_FLOOR: usize =
             OCTET_POINT_G1_LENGTH * 2 + OCTET_SCALAR_LENGTH * 3;
