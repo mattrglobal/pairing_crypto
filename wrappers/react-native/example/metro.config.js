0d9d3c234e67392dbbc6c657129330525d95bb8b
Thomas Pearson <119269574+thomas-pearson-mattr@users.noreply.github.com>
2024-12-05 14:38:05 +1300

    chore(react-native-example): migrate IOS to react native 0.72.14
    React native was upgraded but wasn't migrated when updating from 0.68.2
    
    Have applied the changes for IOS
    https://react-native-community.github.io/upgrade-helper/?from=0.68.2&to=0.72.14

--- wrappers/react-native/example/metro.config.js
+++ wrappers/react-native/example/metro.config.js
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2022 - MATTR Limited
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 const path = require('path');
 const escape = require('escape-string-regexp');
 const exclusionList = require('metro-config/src/defaults/exclusionList');
@@ -9,7 +22,13 @@ const modules = Object.keys({
   ...pak.peerDependencies,
 });
 
-module.exports = {
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
   projectRoot: __dirname,
   watchFolders: [root],
 
@@ -41,3 +60,5 @@ module.exports = {
     }),
   },
 };
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
