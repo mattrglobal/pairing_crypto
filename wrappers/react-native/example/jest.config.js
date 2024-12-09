3d38a7c2ce82f545f439b78c0d4c71a72c9b8ac6
Thomas Pearson <119269574+thomas-pearson-mattr@users.noreply.github.com>
2024-12-09 17:10:34 +1300

    build(deps): upgrade & migrate to detox 20
    IOS detox tests kept failing due to this issue
    https://github.com/wix/Detox/issues/4148.
    
    Upgrading resolves the issue and things are bit faster now!

--- wrappers/react-native/example/jest.config.js
+++ wrappers/react-native/example/jest.config.js
@@ -1,15 +1,29 @@
+/*
+ * Copyright 2022 - MATTR Limited
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 const package = require('./package.json');
 
 module.exports = {
   displayName: package.name,
   preset: 'react-native',
-  reporters: ['detox/runners/jest/streamlineReporter'],
+  maxWorkers: 1,
+  testTimeout: 120000,
+  verbose: true,
+  reporters: ['detox/runners/jest/reporter'],
+  globalSetup: 'detox/runners/jest/globalSetup',
+  globalTeardown: 'detox/runners/jest/globalTeardown',
   testEnvironment: './__tests__/detox-environment',
-  testRunner: 'jest-circus/runner',
-  testTimeout: 20000,
   testMatch: ['**/*.e2e.ts'],
   testPathIgnorePatterns: ['/node_modules/', '/lib/'],
-  verbose: true,
   collectCoverage: true,
   coverageDirectory: './jest_results/coverage/',
   coverageReporters: [['lcov', { projectRoot: '../' }], 'text'],
