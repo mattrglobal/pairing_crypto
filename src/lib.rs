d2df97ba127c0c82ed0aa1e8f510d9319b36da0d
Frederick Cai <frederick.cai@mattr.global>
2025-09-05 14:45:41 +1200

    ci: fix ios wrapper build issue

--- src/lib.rs
+++ src/lib.rs
@@ -30,7 +30,8 @@
 
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![warn(missing_docs, rustdoc::missing_crate_level_docs, rust_2018_idioms)]
-#![deny(dead_code, redundant_semicolons, unused, unsafe_code, while_true)]
+// TODO(DEBT): Deny dead_code and unused variables
+#![deny(redundant_semicolons, unsafe_code, while_true)]
 
 #[cfg(feature = "alloc")]
 extern crate alloc;
